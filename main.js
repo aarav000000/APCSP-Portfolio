96b4a6330f0455152afee7ba4049fc2aef793a5f
aarav000000 <114681964+aarav000000@users.noreply.github.com>
2023-10-16 18:43:23 +0000

    Added stars and project 1

--- main.js
+++ main.js
@@ -4,8 +4,93 @@
 
 // Our stylesheet.
 import './style.css'
+//from chat.gpt//
+// JavaScript
+function createStars() {
+    const body = document.body; // Target the body element
 
+    for (let i = 0; i < 100; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        body.appendChild(star);
 
+        // Randomly position stars initially
+        const x = Math.random() * 100;
+        const y = Math.random() * 100;
+        star.style.left = `${x}%`;
+        star.style.top = `${y}%`;
+
+        // Randomize animation delay for a more realistic effect
+        const delay = Math.random() * 5;
+        star.style.animationDelay = `${delay}s`;
+    }
+}
+
+createStars();
+
+// Animate stars
+function animateStars() {
+    const stars = document.querySelectorAll('.star');
+
+    stars.forEach((star) => {
+        const duration = Math.random() * 5 + 2; // Random animation duration
+        star.style.animation = `twinkle ${duration}s linear infinite alternate`;
+      
+      
+        // Randomly change star position after each animation iteration
+        setTimeout(() => {
+            const x = Math.random() * 100;
+            const y = Math.random() * 100;
+            star.style.left = `${x}%`;
+            star.style.top = `${y}%`;
+        }, Math.random() * duration * 1000);
+      // Delay the position change randomly
+    });
+}
+
+animateStars();
+
+let rows = document.querySelectorAll('.card')
+console.log(rows)
+for (let div of rows){
+  div.addEventListener(
+    "click",
+    function(e){
+      e.target.closest('.card').classList
+      .toggle('active');
+  
+    }
+  );
+}
+//Name spinning from chat GPT//
+let angle = 0;
+//const radius = 150;
+// set radius to be proportional to window width
+
+const speed = 0.02;
+
+function animateNames() {
+  const radius = window.innerWidth * 0.15;
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+
+    const x1 = radius * Math.cos(angle);
+    const y1 = radius * Math.sin(angle);
+    const x2 = -x1;
+    const y2 = -y1;
+
+    firstName.style.transform = `translate(${x1}px, ${y1}px)`;
+    lastName.style.transform = `translate(${x2}px, ${y2}px)`;
+
+    angle += speed;
+    requestAnimationFrame(test);
+}
+
+function test() {
+  animateNames()
+
+}
+animateNames();
 
 `
 // We can import raw code fragments for samples, like this:
